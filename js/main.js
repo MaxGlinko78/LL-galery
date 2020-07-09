791ce23efac75b42fc22abd08b76c9b42557ff8a
Maksim <64214493+MaxGlinko78@users.noreply.github.com>
2020-07-09 11:25:44 +0300

    I'STUPID

--- js/main.js
+++ js/main.js
@@ -2,24 +2,33 @@ const cards = document.querySelectorAll('.card');
 
 for (let i = 0; i < cards.length; i++) {
     const card = cards[i];
-  card.addEventListener('mousemove', startRotate);
-  card.addEventListener('mouseout', stopRotate);
+    card.addEventListener('mousemove', startRotate);
+    card.addEventListener('mouseout', stopRotate);
 
 }
 
 
 
 
-function startRotate(event){
+function startRotate(event) {
     const cardItem = this.querySelector('.card-item');
-    const halfHeight = cardItem.offsetHeight /2;
-    const halfWidth = cardItem.offsetWidth /2;
-    cardItem.style.transform = 'rotateX('+-(event.offsetY - halfHeight)/10+'deg) rotateY('+(event.offsetX - halfWidth)/5 +'deg)';
+    const halfHeight = cardItem.offsetHeight / 2;
+    const halfWidth = cardItem.offsetWidth / 2;
+    cardItem.style.transform = 'rotateX(' + -(event.offsetY - halfHeight) / 10 + 'deg) rotateY(' + (event.offsetX - halfWidth) / 5 + 'deg)';
 
 }
-function stopRotate(event){
+
+function stopRotate(event) {
     const cardItem = this.querySelector('.card-item');
-    
+
     cardItem.style.transform = 'rotate(0)';
 
 }
+
+$('.menu-btn').on('click', function (e) {
+    e.preventDefault();
+    $('.menu').toggleClass('menu_active');
+    $('.content').toggleClass('content_active');
+
+
+})
